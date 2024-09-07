82a1d2bf987e4fecb645ccbcec0b54986ab1a699
VinhTungg <vtung07072000@gmail.com>
2024-09-07 11:46:55 +0700

    added in 7/9/2024

--- CPP0722 - Day So Catalan.cpp
+++ CPP0722 - Day So Catalan.cpp
@@ -13,53 +13,47 @@
 #define MOD 1000000007
 
 using namespace std;
-vector<string> v(5002, "0");
 
-string add(string a, string b) {
-	if (a.length() < b.length())swap(a, b);
-	while (a.length() != b.length())b.insert(0, "0");
-	string c;
-	int nho = 0;
-	for (int i = a.length() - 1; i >= 0; i--) {
-		int x = a[i] + b[i] - 96 + nho;
-		nho = x / 10;
-		int z = x % 10;
-		c.push_back(z + '0');
-	}
-	if (nho > 0)c.push_back(nho + '0');
-    reverse(c.begin(), c.end());
-	return c;
-}
-
-string mul(string s1, string s2){
-    vector<string> v(10, "0");
-    v[1] = s2;
-    foru(i, 2, 9) v[i] = add(v[i - 1], s2);
-    string res;
-    foru(i, 0, s1.size() - 1){
-        res.pb('0');
-        res = add(res, v[s1[i] - '0']);
+string mul(string num1, int num2){
+    if(num1 == "0" || num2 == 0) return "0";
+    string ans(num1.size(), '0');
+    int nho = 0;
+    for(int i = num1.size() - 1; i >= 0; --i){
+        nho = num2 * (num1[i] - '0') + nho;
+        ans[i] = nho % 10 + '0';
+        if(nho) nho /= 10;
     }
-    return res;
+    if(nho) ans = to_string(nho) + ans;
+    return ans;
 }
 
-void init(){
-    v[0] = v[1] = "1";
-    for(int i = 2; i <= 5001; ++i){
-        v[i] = "0";
-        for(int j = 0; j < i; ++j){
-            v[i] = add(v[i], mul(v[j], v[i - j - 1]));
+string solve(int n){
+    vector<int> v;
+    for(int i = n + 2; i <= 2 * n; ++i){
+        v.pb(i);
+    }
+    for(int i = 2; i <= n; ++i){
+        int tmp = i;
+        for(int j = 0; j < v.size() && tmp > 1; ++j){
+            int uoc = __gcd(v[j], tmp);
+            v[j] /= uoc;
+            tmp /= uoc;
         }
     }
+    string ans = "1";
+    for(int i = 0; i < v.size(); ++i){ 
+        ans = mul(ans, v[i]);
+    }
+    return ans;
 }
 
 int main(){
     boost;
-    int t = 1; 
+    //int t = 1; 
     //cin >> t;
-    init();
-    while(t--){
-        int n; cin >> n;
-        cout << v[n + 1] << endl;
-    }
+    //while(t--){
+        short int n;
+        cin >> n;
+        cout << solve(n + 1);
+    //}
 }
