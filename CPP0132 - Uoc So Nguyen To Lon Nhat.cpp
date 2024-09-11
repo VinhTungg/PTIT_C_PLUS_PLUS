2c57d414b8284ea2507d9a23a83db8393b23edd6
VinhTungg <vtung07072000@gmail.com>
2024-09-11 14:20:41 +0700

    added in 11/9/2024

--- CPP0132 - Uoc So Nguyen To Lon Nhat.cpp
+++ CPP0132 - Uoc So Nguyen To Lon Nhat.cpp
@@ -13,21 +13,46 @@
 #define MOD 1000000007
 
 using namespace std;
+const ll Max = 10000000;
+vector<int> primes;
+
+void sieve() {
+    vector<bool> is_prime(Max + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i <= Max; i++) {
+        if (is_prime[i]) {
+            primes.push_back(i);
+            for (int j = i * 2; j <= Max; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+}
+
+void solve(ll n){
+    ll cnt;
+    for (int i : primes) {
+        if (i * i > n) break;
+        while (n % i == 0) {
+            cnt = i;
+            n /= i;
+        }
+    }
+    if (n > 1) {
+        cnt = n;
+    }
+    cout << cnt;
+}
 
 int main(){
     boost;
-    int t = 1;
+    sieve();
+    int t;
     cin >> t;
-    while(t--){
-    	ll n; cin >> n;
-        ll ans;
-        foru(i, 2, n){
-            while(n % i == 0){
-                ans = i;
-                n /= i;
-            }
-        }
-        if(n > 1) ans = n;
-        cout << ans << endl;
-	}
+    while (t--) {
+        ll n;
+        cin >> n;
+        solve(n);
+        cout << endl;
+    }
 }
